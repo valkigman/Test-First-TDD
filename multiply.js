ef6e461449bfdc788a7f8614fa0699a603d793b6
KIGMAN <Valentine Kiguli>
2024-07-07 23:37:43 -0700

    General commit

---
+++ multiply.js
@@ -0,0 +1,33 @@
+// *Cycle 1(rectified)*
+
+export function multiply(a,b){
+    return 1;
+}
+
+
+// *Cycle 2*
+
+// export function multiply(a,b){
+//     return a*b;
+// }
+
+
+// *Cycle 3*
+
+// export function multiply(a,b){
+//     return a*b;
+// }
+
+
+// *Cycle 4*
+
+// export function multiply(a,b){
+//     return a*b;
+// }
+
+
+// *Cycle 5*
+
+// export function multiply(a,b){
+//     return a*b;
+// }
---
+++ test/test.js
@@ -0,0 +1,68 @@
+// *Cycle 1*
+
+import {assert} from 'chai';
+
+describe('Multiplier', function(){
+    it('multiply should return product', function(){
+        assert.equal(multiply(1,1), 1);
+    });
+});
+
+
+// *Cycle 1 (rectified)*
+
+// import {assert} from 'chai';
+// import {multiply} from '../multiply.js';
+
+// describe('Multiplier', function(){
+//     it('multiply should return product', function(){
+//         assert.equal(multiply(1,1), 1);
+//     });
+// });
+
+
+// *Cycle 2*
+
+// import {assert} from 'chai';
+// import {multiply} from '../multiply.js';
+
+// describe('Multiplier', function(){
+//     it('multiply should return product', function(){
+//         assert.equal(multiply(2,2), 4);
+//     });
+// });
+
+
+// *Cycle 3*
+
+// import {assert} from 'chai';
+// import {multiply} from '../multiply.js';
+
+// describe('Multiplier', function(){
+//     it('multiply should return product', function(){
+//         assert.equal(multiply(3,3), 9);
+//     });
+// });
+
+
+// *Cycle 4*
+
+// import {assert} from 'chai';
+// import {multiply} from '../multiply.js';
+
+// describe('Multiplier', function(){
+//     it('multiply should return product', function(){
+//         assert.equal(multiply(4,4), 16);
+//     });
+// });
+
+// *Cycle 5*
+
+// import {assert} from 'chai';
+// import {multiply} from '../multiply.js';
+
+// describe('Multiplier', function(){
+//     it('multiply should return product', function(){
+//         assert.equal(multiply(23,45), 23*45);
+//     });
+// });
